39d54a5c4442b1be5e6a50c180dbba12cc22a6a0
Shurman Hung <shurman_h@hotmail.com>
2022-06-11 18:20:45 +0800

    [20220611] Add admin role feature / UI decoration / Document update

--- README.md
+++ README.md
@@ -8,8 +8,48 @@
 ### Login URL
 ```http://[your ip]:[port]/ticket```
 
-### For QA
-1.
+### Test Accouts & Password
+* ```RD``` / ```rd123456```
+* ```QA``` / ```QA123456```
+* ```PM``` / ```pm123456```
+* ```ADMIN``` / ```admin123456```
 
-### For RD
-1.
+### Phase I usage flow
+1. QA Creates ticket by filling in [New Ticket] page
+1. RD switches ticket to "Resolve"
+1. QA can swith to [Close] if QA acknowledges. If not, QA may switch back to [Open]
+
+### Phase II usage flow
+
+## System Architecture
+* User
+  - Uses django default model
+  - Groups including: RD, QA, PM, 
+* Ticket Class
+  - title (string)
+  - description (string)
+  - status (enum)
+  - t_type (enum)
+  - severity (enum)
+  - creator (django User class)
+  - create_date (datetime)
+* Pages
+  - Login
+  - Ticket List
+  - Ticket Detail
+
+## API Implementation Plan
+* Authentication with access token in header
+
+```
+get_tickets()
+```
+```
+get_ticket(int id)
+```
+```
+create_ticket(Ticket ticket)
+```
+```
+modify_ticket(int id, Ticket new_ticket)
+```
--- ticketsys/implement/views.py
+++ ticketsys/implement/views.py
@@ -45,18 +45,21 @@ def ticket_new(request):
     is_rd = False
     is_qa = False
     is_pm = False
+    is_adm = False
     if request.user.groups.filter(name='RD').exists():
         is_rd = True
     if request.user.groups.filter(name='QA').exists():
         is_qa = True
     if request.user.groups.filter(name='PM').exists():
         is_pm = True
+    if request.user.groups.filter(name='Administrator').exists():
+        is_adm = True
 
     if request.method == 'POST':
         if is_qa or is_pm:
             newticket = Ticket()
             newticket.title = request.POST.get('title','')
-            newticket.t_type = '0' if is_qa else '1'
+            newticket.t_type = '0' if is_qa else request.POST.get('t_type','')
             newticket.status = '0'
             newticket.severity = request.POST.get('severity','')
             newticket.description = request.POST.get('description','')
@@ -75,15 +78,23 @@ def ticket_show(request, tid):
     is_rd = False
     is_qa = False
     is_pm = False
+    is_adm = False
+    status_flag = False
+    info_flag = False
+
     if request.user.groups.filter(name='RD').exists():
         is_rd = True
     if request.user.groups.filter(name='QA').exists():
         is_qa = True
     if request.user.groups.filter(name='PM').exists():
         is_pm = True
+    if request.user.groups.filter(name='Administrator').exists():
+        is_adm = True
 
     if request.method == 'POST':
-        if is_qa:
+        if is_adm or (((is_qa and ticket.t_type == '0') or (is_pm and ticket.t_type == '1')) and ticket.status != '2'):
+            if is_adm:
+                ticket.t_type = request.POST.get('t_type','')
             ticket.title = request.POST.get('title','')
             ticket.status = request.POST.get('status','')
             ticket.severity = request.POST.get('severity','')
@@ -98,5 +109,10 @@ def ticket_show(request, tid):
 
     if ticket is None:
         return HttpResponseRedirect(reverse('user_main'))
+
+    if is_adm or (((is_qa and (ticket.t_type == '0' or ticket.t_type == '2')) or (is_pm and ticket.t_type == '1')) and ticket.status != '2'):
+        info_flag = True
+    if is_adm or ((is_qa and ticket.t_type == '0' and ticket.status == '1') or (is_qa and ticket.t_type == '2') or (is_pm and ticket.t_type == '1' and ticket.status == '1') or (is_rd and ticket.t_type != '2' and ticket.status != '2')):
+        status_flag = True
 
     return render(request, 'ticket_main.html', locals())
